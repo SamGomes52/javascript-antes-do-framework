645eb3604979591a831865d49c350ee0414744ad
SamGomes52 <gomesamantha2303@gmail.com>
2022-12-21 12:22:18 -0300

    Arrow functions e eventos

--- 01-tudo-e-objeto/script.js
+++ 01-tudo-e-objeto/script.js
@@ -1,20 +1,21 @@
-// const menu = {
-//   class: ".principal",
-//   ativar() {
-//     const menuElement = document.querySelector(this.class);
-//     menuElement.classList.add("ativo");
-//   }
-// };
-
-// const body = document.querySelector("body");
-// body.classList.add("js");
-
 const menu = {
   class: ".principal",
   ativar() {
     const menuElement = document.querySelector(this.class);
     console.log(menuElement);
+
+    menuElement.classList.add("ativo");
+    console.log(menuElement.classList);
+
+    const ativo = document.querySelector(".ativo");
+    console.log(ativo);
   }
 };
 
 menu.ativar();
+
+const body = document.querySelector("body");
+body.classList.add("js");
+console.log(body.classList)
+
+
--- 02-arrow-function/script.js
+++ 02-arrow-function/script.js
@@ -1,23 +1,22 @@
-// class Menu {
-//   constructor(menu) {
-//     this.menuElement = document.querySelector(menu);
-//     this.activeClass = "active";
-//   }
-//   addActiveEvent() {
-//     this.menuElement.addEventListener("click", event =>
-//       event.target.classList.add(this.activeClass)
-//     );
-//   }
-// }
-// // teste
-// const menu = new Menu(".principal");
-// menu.addActiveEvent();
+// Forma convencional de função: 
+function upperCase(nome) {
+  return nome.toUpperCase();
+}
 
-// const upperName = name => name.toUpperCase();
-// const countLetters = word => word.length;
+// Colocando a função dentro de uma variável ou constante:
+const stringLength = function(nome) {
+  return nome.length;
+}
 
-// console.log(countLetters("Andre"));
+// Arrow function:
+const lowerCase = (nome) => {
+  return nome.toLowerCase();
+}
 
+// Em uma só linha:
+const replaceString = (nome, remove, sub) => nome.replace(remove, sub);
+
+// Exemplo de classe usando arrow function:
 class Menu {
   constructor(menu) {
     this.menuElement = document.querySelector(menu);
@@ -33,3 +32,34 @@ class Menu {
 
 const menu = new Menu(".principal");
 menu.addActiveEvent();
+
+
+class Aviso {
+  constructor(aviso) {
+    this.avisoElement = document.querySelector(aviso);
+    this.visivelClass = "visivel";
+  }
+
+  tornaVisivel() {
+    this.avisoElement.classList.add(this.visivelClass);
+  }
+}
+
+class Informacao {
+  constructor(informacao) {
+    this.informacaoElement = document.querySelector(informacao);
+    this.copiadoClass = "copiado";
+  }
+
+  addActiveEvent(avisoClass) {
+    this.informacaoElement.addEventListener("copy", (event) => {
+      console.log(this);
+      event.target.classList.add(this.copiadoClass);
+      avisoClass.tornaVisivel();
+    });
+  }
+}
+
+const aviso = new Aviso(".invisivel");
+const informacao = new Informacao(".texto");
+informacao.addActiveEvent(aviso);
